9544146bfb8e63f11e8b8297e1cda7790d6d212b
TranLeDong <mobai54321@gmail.com>
2024-02-25 19:52:04 +0700

    editted

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
+// Elements selection
 const toggleNavBtn = document.querySelector(".toggleNavBtn");
 const navbarItems = document.querySelector(".navbar__nav__items");
+const categoryContainer = document.querySelector(".categoryContainer");
 
+// variable init
+let currentCategory = "All";
+
+// 3-line button that toggle navigation in desktop view
 toggleNavBtn.addEventListener("click", () => {
   navbarItems.classList.toggle("hidden");
 });
 
-const getJSON = async () => {
+// Get categories from JSON and print to html
+const getCategories = async () => {
+  // Get projects data from JSON
   const response = await fetch("./assets/projects.json");
   const projects = await response.json();
-  console.log(projects);
+
+  // print 'all' category which is not in JSON
+  categoryContainer.insertAdjacentHTML(
+    "beforeend",
+    '<button class="DMsans category_button activeCategory">All</button>'
+  );
+
+  // print each categories
+  projects.forEach((proj) => {
+    categoryContainer.insertAdjacentHTML(
+      "beforeend",
+      `<button class="DMsans category_button">${proj.category}</button>`
+    );
+  });
 };
-getJSON();
+getCategories();
